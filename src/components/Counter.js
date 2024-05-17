ec0665a72fb6ec4249b4ef61d46076136d963c01
Meda Kishan <meda.kishan@incture.com>
2024-05-17 04:16:03 +0000

    word counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,17 +1,68 @@
 import React ,{useState} from "react"
 import PropTypes from 'prop-types';
 
-
 export default function Myfunc(props)
-{    
+{
+
+    const [text,setText]=useState("");
+
+    const handleClick_uppercase=() =>{
+        console.log("button is clicked");
+        let upperText=text.toUpperCase();
+        setText(upperText);
+    }
+
+    const handleClick_lowercase=() =>{
+        let lowerText=text.toLowerCase();
+        setText(lowerText);
+    }
+
+    const handleChange=(event)=>{
+         console.log("Tries to change the input value");
+         setText(event.target.value);
+    }
+
+    const handleClick_clearText=() =>{
+        setText("");
+    }
+    
     return (
         <>
-        <div>
+        {/* <div>
             <h1> {props.heading} </h1>
             <p>This is the paragraph text</p>
             <p>{props.paraText}</p>
 
+        </div> */}
+
+        <div className="main_box">
+            <div>
+                <h1>Enter the text </h1>
+                <textarea className="txt_area" id="myBox" rows="15" value={text} onChange={handleChange}> </textarea>
+            </div>
+
+            <div className="buttons">
+                <button onClick={handleClick_uppercase}>Convert to Uppercase</button>
+
+                <button onClick={handleClick_lowercase}>Convert to Lowercase</button>
+
+                <button onClick={handleClick_clearText}>Clear Text</button>
+            </div>
+
+            <div className="container">
+                 <h1>{props.summary}</h1>
+                 <p>{text.split(" ").length} words and {text.length} characters</p>
+                 <p>{0.008 * text.split(" ").length} Minutes to read</p>
+
+            </div>
+
+            <div className="preview">
+                <h2>Preview</h2>
+                <p>{text}</p>
+            </div>
+
         </div>
+         
         </>
     )
 };
